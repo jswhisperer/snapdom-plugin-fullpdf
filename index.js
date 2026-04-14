1174a21792b22e5aaccc2cc5d3223869be18bbd7
Gregory JSWhisperer <hello@jswhisperer.uk>
2026-04-14 16:01:45 +0100

    renaming

--- index.js
+++ index.js
@@ -1,17 +1,21 @@
 import html2pdf from "html2pdf.js"
 
-export function realPdf(options = {}) {
+export function html2js(options = {}) {
   return {
-    name: "fullPdf",
+    name: "html2js",
     defineExports() {
       return {
-        RealPdf: async (ctx, opts = {}) => {
+        html2js: async (ctx, opts = {}) => {
           const bodyElement = opts.element || document.querySelector("body")
           if (bodyElement) {
-            const pdf = await html2pdf()
-              .set(options)
-              .from(bodyElement)
-              .save()
+            try {
+                await html2pdf()
+                    .set(options)
+                    .from(bodyElement)
+                    .save()
+            } catch(e) {
+                console.error(e)
+            }
           }
         }
       }
@@ -19,4 +23,4 @@ export function realPdf(options = {}) {
   }
 }
 
-export default realPdf
+export default html2js
